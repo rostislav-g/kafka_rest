e9c95acd3281ed26feab32c9cf638fc1393c4711
rostislav-g <rippastiy@gmail.com>
2020-09-23 16:05:14 +0300

    added some implementations for Prodcer

--- consumer.py
+++ consumer.py
@@ -1,7 +1,7 @@
 from kafka import KafkaConsumer
 from db import DatabaseConnector
 import requests
-from json import loads
+import json
 
 
 class Consumer:
@@ -15,7 +15,7 @@ class Consumer:
 
     def _consume_from_kafka(self) -> None:
         # todo: handle ssl parameters
-        consumer = KafkaConsumer(**self.properties['consumer'])
+        consumer = KafkaConsumer(**self._properties['consumer'])
         # todo: logging consumer initialization
 
         for record in consumer:
@@ -26,7 +26,6 @@ class Consumer:
 
     def http_post(self, data: str) -> None:
         # todo: logging "locally" -> f'Triggering DAG with the following data: {data}'
-        # todo: how to get dag_name? (val airflowURL)
         dag_name = self._db.get_config_from_db(self.get_message_type(data=data))
         airflow_url = f'{self._airflow}/api/experimental/dags/{dag_name}/dag_runs'
 
@@ -40,17 +39,17 @@ class Consumer:
 
         if response.ok:
             # todo: logging "locally" -> "Triggered Airflow DAG successfully"
-            # todo: DatabaseConnector(configuration).logToDB('INFO', data, '')
-            pass
+            self._db.log_to_db('INFO', data, '')
         else:
             # todo: logging "locally" -> "Error while triggering Airflow DAG"
-            # todo: DatabaseConnector(configurtion).logToDB('ERROR', data, 'Error while Triggering Airflow DAG')
-            pass
+            self._db.log_to_db('ERROR', data, 'Error while triggering Airflow DAG')
 
-    # todo: add implementation
+    # todo: check
     @staticmethod
-    def get_message_type(data):
-        pass
+    def get_message_type(data: str) -> str:
+        message_type = json.loads()['message_type']
+        # todo: add "locally" logging -> f"Got message_type from kafka: {message_type}"
+        return message_type
 
     @staticmethod
     def _get_properties(config):
--- producer.py
+++ producer.py
@@ -1,27 +1,34 @@
 from kafka import KafkaProducer
+from db import DatabaseConnector
 import requests
 from json import loads
 
 
 class Producer:
     def __init__(self, config):
-        self.properties = self._get_properties(config)
-        self.airflow = config['airflow']
-        # todo: Add DatabaseConnector implementation
-        self.db = None
+        self._properties = self._get_properties(config)
+        self._airflow = config['airflow']
+        self._db = DatabaseConnector(config)
 
     def produce_to_kafka(self, data):
-        # todo: add implementation of callback function
-        callback = None
-        topic = self.properties['producer']['topic']
-
-        producer = KafkaProducer(**self.properties['producer'])
-        producer \
-            .send(topic=topic, value=data) \
-            .add_callback(callback, **{'callback_parameter': 'value'})
+        topic = self._properties['producer']['topic']
+        producer = KafkaProducer(**self._properties['producer'])
+        # todo: add "locally" logging -> "Sending data to kafka.."
+        f = producer.send(topic=topic, value=data) \
+            .add_callback(self._on_success, **{'data': data}) \
+            .add_errback(self._on_failure, **{'data': data})
 
     def run(self):
         pass
+
+    def _on_success(self, metadata, data=None) -> None:
+        # todo: add "locally" logging -> f"Successfully sent message: {metadata}"
+        self._db.log_to_db('INFO', data, '')
+
+    def _on_failure(self, exception, data=None) -> None:
+        # todo: add "locally" logging -> f"Error while attempting to send message to kafka: {ex}"
+        self._db.log_to_db('ERROR', data, str(exception))
+
 
     @staticmethod
     def _get_properties(config):
